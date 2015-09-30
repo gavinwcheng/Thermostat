88752e917edd8784517996903dda82b50a872fd6
Harry <harryandrew.dix@gmail.com>
2015-09-30 09:08:07 +0100

    adds index.html & style.css

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -52,26 +52,15 @@ var thermostat
     expect(thermostat.temperature).toEqual(20);
   });
 
-  it('',function() {
-
+  it('color changes to green when at or below 18',function() {
+    for (t = 0; t < 2; t++) {
+      thermostat.downButton();
+    }
+    expect(thermostat.colour).toEqual("green")
   });
 
+  xit('',function() {});
+  xit('',function() {});
+  xit('',function() {});
+
 });
-
-// describe("clicking a thing makes it go up by 1", function() {
-//   beforeEach(function() {
-//     // set up your ‘fixture’ i.e. the HTML/JS for the feature
-//   });
-
-//   it("increments a counter", function() {
-//     // use jQuery to fetch the HTML element with the id=“counter"
-//     var $counter = $('#counter');
-//     var $button = $('#button');
-
-//     // click the button
-//     $button.click();
-//     // expect the thing to now show 1
-//     expect($counter.text()).toContainText("1");
-//     // that toContainText is a Jasmine-jQuery matcher
-//   });
-// });
--- src/javascripts/Thermostat.js
+++ src/javascripts/Thermostat.js
@@ -1,8 +1,8 @@
 function Thermostat(){
-  this.temperature = 20
-  this.minTemp = 10
-  this.maxTemp = 25
-  this.powerSave = true
+  this.temperature = 20;
+  this.minTemp = 10;
+  this.maxTemp = 25;
+  this.powerSave = true;
 }
 
 Thermostat.prototype.upButton = function() {
@@ -13,26 +13,38 @@ Thermostat.prototype.upButton = function() {
     throw "Temperature cannot be set above 32 degrees";
 }
   return this.temperature += 1;
-}
+};
 
 Thermostat.prototype.downButton = function() {
   if (this.temperature <= 10) {
     throw "Temperature cannot be set below 10 degrees";
   }
   return this.temperature -= 1;
-}
+};
 
 Thermostat.prototype.powerSaveSet = function(value) {
   if (value === true) {
-    this.powerSave = true
+    this.powerSave = true;
     return this.maxTemp = 25;
   }
   if (value === false) {
-    this.powerSave = false
+    this.powerSave = false;
     return this.maxTemp = 32;
   }
-}
+};
 
 Thermostat.prototype.resetTemperature = function() {
-  this.temperature = 20
-}
+  this.temperature = 20;
+};
+
+Thermostat.prototype.colour = function() {
+  if (this.temperature <= 18) {
+    return "green";
+  }
+  if (this.temperature >= 25) {
+    return "red";
+  }
+  else {
+    return "orange";
+  }
+};
