d6d507cc1a0864f4334ae1c195e394b92f5cd2fa
Harry <harryandrew.dix@gmail.com>
2015-09-30 12:24:46 +0100

    app.js created

--- src/javascripts/Thermostat.js
+++ src/javascripts/Thermostat.js
@@ -3,22 +3,22 @@ function Thermostat(){
   this.minTemp = 10;
   this.maxTemp = 25;
   this.powerSave = true;
-}
+};
 
 Thermostat.prototype.upButton = function() {
   if (this.powerSave === true && this.temperature >= 25) {
     throw "Temperature cannot be set above 25 degrees";
-}
+};
   if (this.powerSave === false && this.temperature >= 32) {
     throw "Temperature cannot be set above 32 degrees";
-}
+};
   return this.temperature += 1;
 };
 
 Thermostat.prototype.downButton = function() {
   if (this.temperature <= 10) {
     throw "Temperature cannot be set below 10 degrees";
-  }
+  };
   return this.temperature -= 1;
 };
 
@@ -26,11 +26,11 @@ Thermostat.prototype.powerSaveSet = function(value) {
   if (value === true) {
     this.powerSave = true;
     return this.maxTemp = 25;
-  }
+  };
   if (value === false) {
     this.powerSave = false;
     return this.maxTemp = 32;
-  }
+  };
 };
 
 Thermostat.prototype.resetTemperature = function() {
@@ -40,7 +40,7 @@ Thermostat.prototype.resetTemperature = function() {
 Thermostat.prototype.colour = function() {
   if (this.temperature <= 18) {
     return "green";
-  }
+  };
   if (this.temperature >= 25) {
     return "red";
   }
